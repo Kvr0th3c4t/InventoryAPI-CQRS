287a2ccdef2d46af596ce2fa614a3eb9df4d92d5
Adrian <adrianc.crim@hotmail.com>
2025-11-18 02:37:59 +0100

    Ajustados cambios en los tests

--- tests/InventoryAPI.IntegrationTests/Infrastructure/IntegrationTestBase.cs
+++ tests/InventoryAPI.IntegrationTests/Infrastructure/IntegrationTestBase.cs
@@ -19,6 +19,8 @@ public class IntegrationTestsBase : IAsyncLifetime
             .WithPassword("Test@12345")
             .Build();
 
+        _msSqlContainer.StartAsync().GetAwaiter().GetResult();
+
         Factory = new WebApplicationFactory<Program>()
             .WithWebHostBuilder(builder =>
             {
